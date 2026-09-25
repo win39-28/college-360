bdad5283ceda541578646ce0d509283d9d8c6fe6
Tu Nombre <tu-correo@ejemplo.com>
2026-09-25 12:09:32 -0400

    bridge between app and PostgreSQL done

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
